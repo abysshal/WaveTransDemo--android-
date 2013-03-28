a42183b9307876db354ad26c24f8a14d1680777b
dreamingfish <dreamingfish123@gmail.com>
2013-03-28 09:40:31 +0800

    bug fixed & add play once button
    update sleep time, correct spelling mistakes, add play once button

--- gen/info/dreamingfish123/wavetransdemo/R.java
+++ gen/info/dreamingfish123/wavetransdemo/R.java
@@ -14,19 +14,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f070008;
-        public static final int Button02=0x7f070006;
-        public static final int Button03=0x7f070009;
-        public static final int Button04=0x7f07000a;
-        public static final int Button05=0x7f07000b;
-        public static final int TextView01=0x7f070005;
-        public static final int TextView02=0x7f070004;
-        public static final int TextView1=0x7f070002;
+        public static final int Button01=0x7f070004;
+        public static final int Button02=0x7f070005;
+        public static final int Button03=0x7f070006;
+        public static final int Button04=0x7f070009;
+        public static final int Button05=0x7f07000a;
+        public static final int TextView01=0x7f070002;
+        public static final int TextView02=0x7f070001;
         public static final int button1=0x7f070000;
+        public static final int button2=0x7f070008;
         public static final int menu_settings=0x7f07000c;
         public static final int textView1=0x7f070003;
-        public static final int textView2=0x7f070001;
-        public static final int textView3=0x7f070007;
+        public static final int textView2=0x7f070007;
+        public static final int textView3=0x7f07000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
